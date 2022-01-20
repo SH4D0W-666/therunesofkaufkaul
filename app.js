aa2830a87052f07681ff3a67ed5f1504c7080679
Amithesh Gupta <amithesh2099@gmail.com>
2022-01-20 13:44:58 +0530

    'msg'

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+
+//https://therunesofkaufkaul.herokuapp.com/ | https://git.heroku.com/therunesofkaufkaul.git
+
 app.get("/", (req, res) => {
-res.send("Hello Heroku");
+	res.send("Hello Heroku");
 })
+
 app.listen(process.env.PORT || port, () => {
-console.log("listening 8080...");
+	console.log("listening 8080...");
 });
